dcdead19d10777e102e9c980a74a2ec5fbb03193
Anya <81263768+anyabuu@users.noreply.github.com>
2025-03-14 18:05:57 +0200

    errors fixed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
-let a = prompt('Enter first name');
-let b = prompt('Enter last name');
-let c = prompt('Enter your age');
+function getFirstName() {
+  let firstName;
 
-alert(`Hi, ${a} ${b}, your age - ${c}`);
+  do {
+    firstName = prompt('Enter first name');
+  } while (firstName === null || !isNaN(firstName) || firstName === '');
+
+  return firstName;
+}
+
+function getLastName() {
+  let lastName;
+
+  do {
+    lastName = prompt('Enter last name');
+  } while (lastName === null || !isNaN(lastName) || lastName === '');
+
+  return lastName;
+}
+
+function getAge() {
+  let age;
+
+  do {
+    age = prompt('Enter your age');
+  } while (isNaN(age) || age === null || age === '');
+
+  return age;
+}
+
+const firstName = getFirstName();
+const lastName = getLastName();
+const age = getAge();
+
+alert(`Hi, ${firstName} ${lastName}, your age - ${age}`);
